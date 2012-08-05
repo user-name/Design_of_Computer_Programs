43c54f26c260af4fa5e5fd87953eec2895f58e81
Or Ange <orangisme@gmail.com>
2012-08-05 22:24:01 +0400

    Write a function, allmax(iterable, key=None), that returns # a list of all items equal to the max of the iterable, # according to the function specified by key.

--- unit1.py
+++ unit1.py
@@ -1,82 +1,85 @@
 # -----------
 # User Instructions
 # 
-# Define a function, two_pair(ranks).
+# Write a function, allmax(iterable, key=None), that returns
+# a list of all items equal to the max of the iterable, 
+# according to the function specified by key. 
 
-def two_pair(ranks):
-# -----------
-# User Instructions
-#
-# Define a function, two_pair(ranks).
+def poker(hands):
+    "Return a list of winning hands: poker([hand,...]) => [hand,...]"
+    return allmax(hands, key=hand_rank)
 
-def two_pair(ranks):
-    """If there are two pair, return the two ranks as a
-    tuple: (highest, lowest); otherwise return None."""
+def allmax(iterable, key=None):
+    "Return a list of all items equal to the max of the iterable."
     # Your code here.
-    if len(set(ranks))==3:
-        for i in set(ranks):
-            ranks.remove(i)
-        if ranks[0]-ranks[1]!=0:
-            return (max(ranks),min(ranks))
-    return None
+    result,maxval=[], None
+    key=key or (lambda x:x)
+    for x in iterable:
+        xval=key(x)
+        if not result or xval>maxval:
+            result, maxval=[x], xval
+        elif xval == maxval:
+            result.append(x)
+    return result
 
-
-def kind(n, ranks):
-    """Return the first rank that this hand has exactly n of.
-    Return None if there is no n-of-a-kind in the hand."""
-    for r in ranks:
-        if ranks.count(r) == n: return r
-    return None
-
-def test():
-    "Test cases for the functions in poker program."
-    sf = "6C 7C 8C 9C TC".split() # Straight Flush
-    fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
-    fh = "TD TC TH 7C 7D".split() # Full House
-    fkranks = card_ranks(fk)
-    tpranks = card_ranks(tp)
-    assert kind(4, fkranks) == 9
-    assert kind(3, fkranks) == None
-    assert kind(2, fkranks) == None
-    assert kind(1, fkranks) == 7
-    return 'tests pass'
+def hand_rank(hand):
+    "Return a value indicating the ranking of a hand."
+    ranks = card_ranks(hand)
+    if straight(ranks) and flush(hand):
+        return (8, max(ranks))
+    elif kind(4, ranks):
+        return (7, kind(4, ranks), kind(1, ranks))
+    elif kind(3, ranks) and kind(2, ranks):
+        return (6, kind(3, ranks), kind(2, ranks))
+    elif flush(hand):
+        return (5, ranks)
+    elif straight(ranks):
+        return (4, max(ranks))
+    elif kind(3, ranks):
+        return (3, kind(3, ranks), ranks)
+    elif two_pair(ranks):
+        return (2, two_pair(ranks), ranks)
+    elif kind(2, ranks):
+        return (1, kind(2, ranks), ranks)
+    else:
+        return (0, ranks)
 
 def card_ranks(hand):
     "Return a list of the ranks, sorted with higher first."
     ranks = ['--23456789TJQKA'.index(r) for r, s in hand]
     ranks.sort(reverse = True)
-    return ranks
-    # Your code here.
-    if len(set(ranks))==3:
-        for i in set(ranks):
-            ranks.remove(i)
-        if ranks[0]-ranks[1]!=0:
-            return (max(ranks),min(ranks))
-    return None
+    return [5, 4, 3, 2, 1] if (ranks == [14, 5, 4, 3, 2]) else ranks
 
+def flush(hand):
+    "Return True if all the cards have the same suit."
+    suits = [s for r,s in hand]
+    return len(set(suits)) == 1
+
+def straight(ranks):
+    "Return True if the ordered ranks form a 5-card straight."
+    return (max(ranks)-min(ranks) == 4) and len(set(ranks)) == 5
 
 def kind(n, ranks):
-    """Return the first rank that this hand has exactly n of.
+    """Return the first rank that this hand has exactly n-of-a-kind of.
     Return None if there is no n-of-a-kind in the hand."""
     for r in ranks:
         if ranks.count(r) == n: return r
     return None
 
+def two_pair(ranks):
+    "If there are two pair here, return the two ranks of the two pairs, else None."
+    pair = kind(2, ranks)
+    lowpair = kind(2, list(reversed(ranks)))
+    if pair and lowpair != pair:
+        return (pair, lowpair)
+    else:
+        return None
+
 def test():
     "Test cases for the functions in poker program."
-    sf = "6C 7C 8C 9C TC".split() # Straight Flush
+    sf1 = "6C 7C 8C 9C TC".split() # Straight Flush
+    sf2 = "6D 7D 8D 9D TD".split() # Straight Flush
     fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
     fh = "TD TC TH 7C 7D".split() # Full House
-    fkranks = card_ranks(fk)
-    tpranks = card_ranks(tp)
-    assert kind(4, fkranks) == 9
-    assert kind(3, fkranks) == None
-    assert kind(2, fkranks) == None
-    assert kind(1, fkranks) == 7
+    assert poker([sf1, sf2, fk, fh]) == [sf1, sf2]
     return 'tests pass'
-
-def card_ranks(hand):
-    "Return a list of the ranks, sorted with higher first."
-    ranks = ['--23456789TJQKA'.index(r) for r, s in hand]
-    ranks.sort(reverse = True)
-    return ranks
